6d8d110fefae95af025528ce302a76a6a8cb3ec0
Deepak Lunked <deepaklunked.m@gmail.com>
2021-10-24 14:23:12 +0530

    Update App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import Content from "./components/content/content";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const BASE_URL = "https://artful-iudex.herokuapp.com";
+
+class App extends Component {
+	initialState = {
+		reactions: null,
+		users: null,
+		userReactions: null,
+        content: [1, 2] // assuming there are only two types of content
+	}
+
+	state = this.initialState;
+
+	async componentDidMount() {
+        axios.get(`${BASE_URL}/reactions`).then((response) => {
+			this.setState({
+				reactions: response.data
+			})
+		})
+		axios.get(`${BASE_URL}/users`).then((response) => {
+			this.setState({
+				users: response.data
+			})
+		})
+		axios.get(`${BASE_URL}/user_content_reactions`).then((response) => {
+			this.setState({
+				userReactions: response.data
+			})
+		})  
+	}
+
+	render() {
+		const { users, reactions, userReactions } = this.state;
+        const content = this.state.content.map((id) => {
+            return <Content
+                key={id}
+                id={id}
+                users={users}
+                reactions={reactions}
+                userReactions={userReactions}
+            />
+        });
+        const showElements = (users && reactions && userReactions)
+		return (
+			<div className="App">
+                { showElements ? content : "Loading.." }
+			</div>
+		);
+	}
 }
 
 export default App;
